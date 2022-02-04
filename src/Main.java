e6d0958ac33ae85f69114663a4f90feca147dacb
gonnatinfo2000 <58569152+gonnatinfo2000@users.noreply.github.com>
2022-02-04 09:13:15 +0000

    Main - Ficha 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
+import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
 	
-	public double areaTriang(double altura, double base) {
+	public static double areaTriang(double altura, double base) {
 		return (altura * base) / 2;
 	}
 	
-	public double areaRetang(double altura, double comprimento) {
+	public static double areaRetang(double altura, double comprimento) {
 		return (altura * comprimento);
 	}
 	
-	public double areaCal(double x, double y, double z) {
+	public static double areaCal(double x, double y, double z) {
 		if(z == 1) {
 			return areaTriang(x,y);
 		}else{
@@ -17,7 +19,7 @@ public class Main {
 		}
 	}
 	
-	public int factor(int n) {
+	public static int factor(int n) {
 		if(n == 0) {
 			return 1;
 		}
@@ -30,7 +32,7 @@ public class Main {
 		return n;
 	}
 	
-	public int factorRecursivo(int n) {
+	public static int factorRecursivo(int n) {
 		if(n == 0) {
 			return 1;
 		}
@@ -38,10 +40,10 @@ public class Main {
 		return n * factorRecursivo(n - 1);
 	}
 	
-	public int numTotalVogais(String frase) {
+	public static int numTotalVogais(String frase) {
 		int num_vogais = 0;
 		for(int i = 0; i < frase.length(); i++) {
-			if(frase.charAt(i) == 'a' || frase.charAt(i) == 'e' || frase.charAt(i) == 'i' || frase.charAt(i) == 'o' || frase.charAt(i) == 'u') {
+			if(frase.charAt(i) == 'a' || frase.charAt(i) == 'A'|| frase.charAt(i) == 'e' || frase.charAt(i) == 'E'|| frase.charAt(i) == 'i' || frase.charAt(i) == 'I'|| frase.charAt(i) == 'o' || frase.charAt(i) == 'O' || frase.charAt(i) == 'u' || frase.charAt(i) == 'U') {
 				System.out.println("Vogal " + frase.charAt(i) + " no index " + i);
 				num_vogais += 1;
 			}
@@ -49,43 +51,51 @@ public class Main {
 		return num_vogais;
 	}
 	
-	public String numVogais(String frase) {
+	public static String numVogais() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Insira uma frase: ");
+		String frase = in.nextLine();
 		int num_vogais_a = 0;
 		int num_vogais_e = 0;
 		int num_vogais_i = 0;
 		int num_vogais_o = 0;
 		int num_vogais_u = 0;
 		for(int i = 0; i < frase.length(); i++) {
-			if(frase.charAt(i) == 'a') {
-				System.out.println("Vogal " + frase.charAt(i) + " no index " + i);
+			if(frase.charAt(i) == 'a' || frase.charAt(i) == 'A') {
 				num_vogais_a += 1;
 			}
-			if(frase.charAt(i) == 'e') {
-				System.out.println("Vogal " + frase.charAt(i) + " no index " + i);
+			if(frase.charAt(i) == 'e' || frase.charAt(i) == 'E') {
 				num_vogais_e += 1;
 			}
-			if(frase.charAt(i) == 'i') {
-				System.out.println("Vogal " + frase.charAt(i) + " no index " + i);
+			if(frase.charAt(i) == 'i' || frase.charAt(i) == 'I') {
 				num_vogais_i += 1;
 			}
-			if(frase.charAt(i) == 'o') {
-				System.out.println("Vogal " + frase.charAt(i) + " no index " + i);
+			if(frase.charAt(i) == 'o' || frase.charAt(i) == 'O') {
 				num_vogais_o += 1;
 			}
-			if(frase.charAt(i) == 'u') {
-				System.out.println("Vogal " + frase.charAt(i) + " no index " + i);
+			if(frase.charAt(i) == 'u' || frase.charAt(i) == 'U') {
 				num_vogais_u += 1;
 			}
 		}
 		return "Vogais a: " + num_vogais_a + "\nVogais e: " + num_vogais_e + "\nVogais i: " + num_vogais_i + "\nVogais o: " + num_vogais_o + "\nVogais u: " + num_vogais_u;
 	}
 	
-	public String tempo(int segundos) {
-		int minutos = segundos / 60;
-		int horas = minutos / 60;
-		int dias = horas / 24;
-		int meses = dias / 30;
-		return "\nMeses: " + meses + "\nDias: " + dias + "\nHoras: " + horas + "\nMinutos: " + minutos + "\nSegundos: " + segundos;
+	public static String tempo() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Insira o numero de segundos: ");
+		int segundos = in.nextInt();
+		int resto = 0;
+		int meses = (segundos / 2592000);
+		int dias = (segundos % 2592000) / 86400;
+		int horas = ((segundos % 2592000) % 86400) / 3600;
+		int minutos = (((segundos % 2592000) % 86400) % 3600) / 60;
+		segundos = (((segundos % 2592000) % 86400) % 3600) % 60;
+		Date d = new Date(segundos); return "\nMeses: " + meses + "\nDias: " + dias + "\nHoras: " + horas + "\nMinutos: " + minutos + "\nSegundos: " + segundos;
 	}
-
+	
+	public static void main(String[] args) {
+		System.out.println(numVogais());
+		System.out.println(tempo());
+	}
+	
 }
